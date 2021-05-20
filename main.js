bb6f56b4fa5390a5401df3690a778c875a045de7
Gamze Güneri <62291935+pinacotheque@users.noreply.github.com>
2021-05-20 20:42:03 +0300

    ignore btn working

--- main.js
+++ main.js
@@ -9,33 +9,41 @@ window.onload = function () {
       const parent = document.getElementById("library-content");
       const booksAsHTML = books.map(
         (book) => `
-        <div class="card col-4">
-          <img src="${book.img}" class="card-img-top" alt="..." />
+        <div class="card col-4 product">
+          <img src="${book.img}" class="card-img-top" style="width: 180px; height: 260px; object-fit:cover;align-items:center;" alt="..." />
           <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">
-            ${book.price}
+            ${book.price} $
             </p>
-            <button type="button" class="add-Chart btn btn-primary ">Add to chart</button>
+            
+            
+            <div class="btn-group" role="group" aria-label="Basic example">
+              
+              <button type="button" class="add-Chart btn btn-danger ">Add to chart</button>
+              <button type="button" class="btn btn-light">Ignore</button>
+            </div>
           </div>
         </div>`
       );
       const libraryContent = booksAsHTML.join("");
       parent.innerHTML = libraryContent;
+      let addCart = document.querySelectorAll(".btn-danger");
+      let forgetAbtit = document.querySelectorAll(".btn-light");
+      let selectedBook = document.querySelectorAll(".product");
+      for (let i = 0; i < addCart.length; i++) {
+        button = addCart[i];
+        button.addEventListener("click", () => {
+          shopCollection.push(selectedBook);
+          button.innerText = "Added to chart";
+          // addToChart();
+          console.log("clicked");
+        });
+        ignoreBtn = forgetAbtit[i];
+        ignoreBtn.addEventListener("click", () => {
+          selectedBook[i].style.display = "none";
+          // addToChart();
+        });
+      }
     });
-
-  let addCart = document.querySelectorAll(".card-body  .add-Chart");
-  console.log(addCart);
-
-  for (let i = 0; i < addCart.length; i++) {
-    button = addCart[i];
-    button.addEventListener("click", () => {
-      // addToChart();
-      console.log("clicked");
-    });
-  }
-
-  function addToChart() {
-    chartCard.addEventListener;
-  }
 };
